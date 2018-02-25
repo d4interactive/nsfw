b3e5f985e9844d65451927e4f3df695fd8723102
Muhammad Azhar <muhammad.azhar109@gmail.com>
2018-02-25 19:22:44 +0500

    Added Setup.py file. (#2)
    * Added gitignore and docker file.
    
    * NudeChecker class added, workers created for the XNXX title extraction.
    
    * Removed proxies and finalized the profanity checker.
    
    * Added setup.py for the package installation instead of manually installing.

---
+++ setup.py
@@ -0,0 +1,36 @@
+import os
+
+from setuptools import setup
+
+ROOT = os.path.dirname(os.path.realpath(__file__))
+
+setup(
+    # Meta data
+    name='nsfw',
+    version='0.1.0',
+    author='Muhammad Azhar',
+    author_email='azhar@contentstudio.io',
+    maintainer='Muhammad Azhar',
+    maintainer_email='azhar@contentstudio.io',
+    url='https://contentstudio.io',
+    description='NSFW content anlayzer',
+    long_description=open(os.path.join(ROOT, 'README.md')).read(),
+    license='MIT License',
+    # Package files
+    packages=[
+        'nsfw',
+    ],
+    include_package_data=True,
+    # Dependencies
+    install_requires=[
+        'requests',
+        'lxml',
+        'nltk',
+        'pybloom_live',
+    ],
+    extras_require={
+        'full': ['urllib3', 'certifi'],
+    },
+    test_suite='nose.collector',
+    tests_require=['nose'],
+)
