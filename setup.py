7acdd603c86b118f912b9959bf65ad24990ffe78
office <azhar@contentstudio.io>
2018-02-26 12:07:02 +0500

    Updated version.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ ROOT = os.path.dirname(os.path.realpath(__file__))
 setup(
     # Meta data
     name='nsfw',
-    version='0.1.0',
+    version='0.1.1',
     author='Muhammad Azhar',
     author_email='azhar@contentstudio.io',
     maintainer='Muhammad Azhar',
